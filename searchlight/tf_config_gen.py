fdabebf91911b2498e5b7ebb2bf4ff28ac70fcdf
Vinay Patel <patelvinay111@gmail.com>
2024-02-21 11:02:13 -0800

    add tf_config_gen

---
+++ searchlight/tf_config_gen.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+
+
+def config_gen(tf_import_blocks_file):
+    terraform_directory = 'sample/'
+
+    # Change to the specified directory
+    os.chdir(terraform_directory)
+
+    # Run the Terraform command
+    terraform_command = 'terraform plan -generate-config-out=generated_config.tf'
+
+    # Use subprocess to run the command
+    process = subprocess.Popen(terraform_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Capture and print the output
+    stdout, stderr = process.communicate()
+    print("Terraform Output:", stdout.decode('utf-8'))
+
+    # Check for errors
+    if process.returncode != 0:
+        print("Error:", stderr.decode('utf-8'))
+    else:
+        print("Terraform command executed successfully.")
