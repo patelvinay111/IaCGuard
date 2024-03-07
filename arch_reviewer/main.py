0cd74a934366383354473621b845d5ba97494cbc
Vinay Patel <patelvinay111@gmail.com>
2024-03-06 18:45:52 -0800

    imggen sample

--- arch_reviewer/main.py
+++ arch_reviewer/main.py
@@ -7,8 +7,8 @@ client = OpenAI(api_key=api_key)
 
 
 # Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
+def encode_image(path):
+    with open(path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
